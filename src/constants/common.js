aaa70dd1dd38def2f38debc1641b30e764571bbc
Benjamin Goh <masotime@gmail.com>
2016-11-13 18:38:38 -0500

    Allow environment to configure port

--- src/constants/common.js
+++ src/constants/common.js
@@ -5,4 +5,4 @@ export const BUNDLE_SOURCE = resolve(process.cwd(), 'src/client/bundle.js');
 export const STATIC_PATHNAME = '/static';
 export const STATIC_SOURCE = resolve(process.cwd(), 'src/static');
 export const SHARED_STATE_NAME = '__MODEL__';
-export const PORT = 8000;
+export const PORT = process.env.PORT || 8000;
